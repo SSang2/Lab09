e6c866f6d63495b3513ead0ac10b374ccfbf4651
isanghyun <21600499@handong.edu>
2018-04-17 21:06:03 +0900

    HW1-6 ArrayList commit

--- src/main/java/edu/handong/csee/java/lab09/MarketingAnalyzer_main.java
+++ src/main/java/edu/handong/csee/java/lab09/MarketingAnalyzer_main.java
@@ -6,59 +6,66 @@ package edu.handong.csee.java.lab09;
  * @author LeeSangHyun
  */
 import java.util.Scanner;		//insert
-
+import java.util.ArrayList;
 public class MarketingAnalyzer_main {		//public main class methodA
 	//variable declaration
 	private double highestSales;		//declare the highest sales variables
 	private double averageSales;		//declare the average sales variables
-	private static Salesman[] team;		//declare the team (salesman) variable
+	ArrayList <Salesman> team = new ArrayList<Salesman>();	//declare the new ArrayList for Salesman
+	//private static Salesman[] team;		//declare the team (salesman) variable
 
 	public static void main(String[] args) {	//public main method 
-		MarketingAnalyzer_main analyzer = new MarketingAnalyzer_main();		//Declaring the constructor
+		MarketingAnalyzer_main analyzer = new MarketingAnalyzer_main();	//Declaring the constructor for new Array List
 		analyzer.getData();		//Call the method getData
 		analyzer.computeAverage();		//Call the method computeAverage
 		analyzer.computeHighestSales();		//Call the method HighestSales
 		analyzer.printResult();				//Call the method Result
 	}
-	public void getData() {		//public void method getting Data
-		System.out.println("Enter the number of salesmen:");		//print out the instruction
+	public void getData() {		//public void method getting
+
 		Scanner myKeyboard = new Scanner(System.in);	//create an object of the Scanner, data enter from the keyboard
 
-		int numofSalesmen = myKeyboard.nextInt();	//read value from the keyboard and assign into number of salesman
+		int i=0;					//declare the integer i variable and save 0;
+		boolean done = false;		//use boolean expression to make two ways for do it or not
+		while(done != false) {		// if its done = true
 
-		team = new Salesman[numofSalesmen];		 //initialize the array of salesman
+			Salesman data = new Salesman();	//declare the new operator Salesman for Salesman data
 
-		for(int i = 0; i < numofSalesmen; i++) {	//repetitive statement for number of salesman
-			System.out.println("Enter data for associate number:" + (i+1));		//print out the instruction
-			myKeyboard.nextInt();		//receiving the output integer data
+			System.out.println("Enter data for associate number:" + (i+1));	//print out the instruction
+			i++;	//increase the value of i one by one for the process
+
 			System.out.println("Name? ");		//print out the instruction
-			myKeyboard.nextLine();		//receiving the output string data
-			String name = myKeyboard.nextLine();	//read value from the keyboard and assign into name
+			String name = myKeyboard.nextLine();	//get the name from the keyboard
+			data.setName(name);			//read value from the keyboard and assign into name
 			System.out.print("Sales? ");		//print out the instruction
 			double sales = myKeyboard.nextDouble();	//read double value from the keyboard and assign into sales
-			team[i] = new Salesman();		//initialize the team by the salesman
-			team[i].setName(name);			//initialize the team by the mutator string value
-			team[i].setSales(sales);		//initialize the team by the mutator integer value
-		}
+			data.setSales(sales);		//read value from the keyboard and assign into sales
+			team.add(data);		//add the specified elements of ArrayList team for data and initial capacity
+			System.out.println("More items for the list? :");	//print out the line for the additional information for item lists
+			myKeyboard.nextLine();		//get the input value from the keyboard in next line
+			String ans = myKeyboard.nextLine();	//get the input value of string character from the keyboard in next line
+			if(!ans.equalsIgnoreCase("yes"))	//if the answer is yes
+				done = true;		//symmetric of done is true, then it gets the continuing process
 
+		}
 	}
 
-	public void computeAverage() {		//public void method for compute the avarage
+	public void computeAverage() {		//public void method for compute the average
 		double sum = 0;		//initialize the variable its null
 
-		for(int i = 0; i < team.length; i++) {		//repetitive statement for sum
-			sum = sum + team[i].getSales();		//calculation of sum
+		for(int i = 0; i < team.size(); i++) {		//repetitive statement for sum
+			sum = sum + team.get(i).getSales();		//calculation of sum
 		}
 
-		averageSales = sum/team.length;		//way of making average sales
+		averageSales = sum/team.size();		//way of making average sales
 	}
 
 	public void computeHighestSales() {		//public void method for highest sales
 
 		double highestSales = 0;		//initialize the variables its null
-		for(int i= 0; i < team.length; i++) {		//repetitive statement for highest sales
-			if(highestSales < team[i].getSales())		//conditional statement for highest sales
-				highestSales = team[i].getSales();		//set the value of variables
+		for(int i= 0; i < team.size(); i++) {		//repetitive statement for highest sales
+			if(highestSales < team.get(i).getSales())		//conditional statement for highest sales
+				highestSales = team.get(i).getSales();		//set the value of variables
 		}
 
 		this.highestSales = highestSales;		//referring the instance variable
@@ -69,10 +76,10 @@ public class MarketingAnalyzer_main {		//public main class methodA
 		System.out.println("Average: " + averageSales);		//print in lines for Average
 		System.out.println("Salesman who has the highest sales: ");		//print in lines for Highest(Sales) Salesman
 
-		for(int i = 0; i < team.length; i++) {		//repetitive statement for result
-			if(team[i].getSales() == highestSales) {	//conditional statement for result
-				System.out.println("Name: " + team[i].getName());	//print in lines for name
-				System.out.println("Sales: " + team[i].getName());	//print in lines for sales
+		for(int i = 0; i < team.size(); i++) {		//repetitive statement for result
+			if(team.get(i).getSales() == highestSales) {	//conditional statement for result
+				System.out.println("Name: " + team.get(i).getName());	//print in lines for name
+				System.out.println("Sales: " + team.get(i).getName());	//print in lines for sales
 			}
 		}
 	}
